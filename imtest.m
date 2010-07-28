05c06ae3a71da9d4d96af09b2e401b30554e2ed2
Andrew E Slaughter <andrew.e.slaughter@gmail.com>
2010-07-28 16:04:19 -0600

    Small change to allows merge back to master

--- imtest.m
+++ imtest.m
@@ -6,6 +6,6 @@ clear classes;
 close all hidden;
 
 % imObject save test
-% imObj = imObject('test.bip');
-imObj = imObject('final_small.jpg');
+imObj = imObject('test.bip');
+%imObj = imObject('final_small.jpg');
 % imObj.saveimObject('saved/test.imobj');
